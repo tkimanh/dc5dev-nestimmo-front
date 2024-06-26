a82c96e8277470c0ad9605ab24df0cd4b74743b9
tkimanh <tran.kimanh@orange.fr>
2024-06-26 19:19:44 +0200

    Ajout de CATEGORY_ENDPOINT dans constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const POST_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/posts`;
+export const CATEGORY_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 export const USER_ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/users`;
