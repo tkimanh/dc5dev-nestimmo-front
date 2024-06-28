1fc79c98984b318d7e15c802084a5edd8bdaa376
tkimanh <tran.kimanh@orange.fr>
2024-06-28 12:27:41 +0200

    modification des imports

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
+import DrawerCategory from "../category/DrawerCategory";
 import DrawerPost from "../post/DrawerPost";
-
 const Navbar = () => {
-    return ( 
-        <nav className="flex justify-between p-5">
-            <Link href="/">Post list</Link>
-            <DrawerPost />
-        </nav>
-     );
-}
- 
+  return (
+    <nav className="flex justify-between p-5">
+      <Link href="/posts">Post list</Link>
+      <Link href="/category">Category list</Link>
+      <DrawerPost />
+      <DrawerCategory />
+    </nav>
+  );
+};
+
 export default Navbar;
--- src/components/post/DrawerPost.tsx
+++ src/components/post/DrawerPost.tsx
@@ -1,44 +1,44 @@
-'use client';
+"use client";
 
 import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from "@/components/ui/drawer"
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 import { Button } from "@/components/ui/button";
-import FormPost from "./FormPost";
 import { useState } from "react";
+import FormPost from "./FormPost";
 
 const DrawerPost = () => {
-    const [open, setOpen] = useState(false);
-
-    return (
-        <Drawer open={open} onOpenChange={setOpen}>
-            <DrawerTrigger asChild>
-                <Button variant="default">
-                    Create new post
-                </Button>
-            </DrawerTrigger>
-            <DrawerContent>
-                <DrawerHeader>
-                    <DrawerTitle className="text-center">Ajouter un post</DrawerTitle>
-                    <DrawerDescription className="text-center">Renseignez l'enssemble des champs.</DrawerDescription>
-                    <FormPost setOpen={setOpen} />
-                </DrawerHeader>
-                <DrawerFooter>
-                    <DrawerClose>
-                        <Button variant="outline">Cancel</Button>
-                    </DrawerClose>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
-    );
-}
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="default">Create new post</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle className="text-center">Ajouter un post</DrawerTitle>
+          <DrawerDescription className="text-center">
+            Renseignez l'ensemble des champs.
+          </DrawerDescription>
+          <FormPost setOpen={setOpen} />
+        </DrawerHeader>
+        <DrawerFooter>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
 export default DrawerPost;
