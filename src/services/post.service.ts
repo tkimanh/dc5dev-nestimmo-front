3652cb4e8b6047d0e2121b9445110872518fe537
tkimanh <tran.kimanh@orange.fr>
2024-06-28 12:28:18 +0200

    modification des espaces

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -2,33 +2,33 @@ import { PostCreateDTO } from "@/types/post";
 import { POST_ENDPOINT } from "@/utils/constants";
 
 export const fetchAllPosts = async () => {
-    const response = await fetch(POST_ENDPOINT);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(POST_ENDPOINT);
+  const data = await response.json();
+  return data;
+};
 
 export const fetchPostById = async (id: string) => {
-    const response = await fetch(`${POST_ENDPOINT}/${id}`);
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(`${POST_ENDPOINT}/${id}`);
+  const data = await response.json();
+  return data;
+};
 
 export const createPost = async (createPostDTO: PostCreateDTO) => {
-    const response = await fetch(POST_ENDPOINT, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(createPostDTO)
-    });
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(POST_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(createPostDTO),
+  });
+  const data = await response.json();
+  return data;
+};
 
 export const deletePost = async (id: string) => {
-    const response = await fetch(`${POST_ENDPOINT}/${id}`, {
-        method: 'DELETE'
-    });
-    const data = await response.json();
-    return data;
-}
+  const response = await fetch(`${POST_ENDPOINT}/${id}`, {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  return data;
+};
